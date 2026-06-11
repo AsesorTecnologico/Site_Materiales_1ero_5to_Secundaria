d443511df3844b3bf81b88e3d32bfd566cead683
AsesorTecnologico <77905882+AsesorTecnologico@users.noreply.github.com>
2026-06-11 09:22:45 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3354,8 +3354,8 @@ const tomos = [
     { id: "tomo-i", name: "Tomo I", icon: "fas fa-book" },
     { id: "tomo-ii", name: "Tomo II", icon: "fas fa-book" },
     { id: "tomo-iii", name: "Tomo III", icon: "fas fa-book" }
-   /* { id: "tomo-iv", name: "Tomo IV", icon: "fas fa-book" },
-    { id: "tomo-v", name: "Tomo V", icon: "fas fa-book" },
+    { id: "tomo-iv", name: "Tomo IV", icon: "fas fa-book" },
+   /* { id: "tomo-v", name: "Tomo V", icon: "fas fa-book" },
     { id: "tomo-vi", name: "Tomo VI", icon: "fas fa-book" },
     { id: "tomo-vii", name: "Tomo VII", icon: "fas fa-book" },
     { id: "tomo-viii", name: "Tomo VIII", icon: "fas fa-book" }*/
